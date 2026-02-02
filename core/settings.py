94a71f8ce6ce58b2780be08b6174b399fcde3c26
AlazarBekele <alazarthe70@gmail.com>
2026-02-02 19:28:58 +0300

    fix static + correct gunicorn path

--- core/settings.py
+++ core/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
